c16995b7718b16900d4e8e8fd4aded6c93b29a15
Paulo César Guillén <pcguillen@gmail.com>
2023-04-01 21:53:14 -0500

    nvm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    starship init fish | source 
+    starship init fish | source
+    set --universal nvm_default_version lts
 end
